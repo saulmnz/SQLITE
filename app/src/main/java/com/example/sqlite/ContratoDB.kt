00c988e012ac05c757b20041295e60404a628277
saulmnz <salvarezmonzon@danielcastelao.org>
2025-12-10 20:03:15 +0100

    PRIMER COMMIT

---
+++ app/src/main/java/com/example/sqlite/ContratoDB.kt
@@ -0,0 +1,13 @@
+package com.example.sqlite
+
+// ARCHIVO: app/src/main/java/com/example/sqlite/ContratoDB.kt
+object ContratoDB {
+    object Estudiantes {
+        // DEFINE _ID MANUALMENTE
+        const val ID = "_id"
+        const val TABLA = "estudiantes"
+        const val COLUMNA_NOMBRE = "nombre"
+        const val COLUMNA_EDAD = "edad"
+        const val COLUMNA_CARRERA = "carrera"
+    }
+}
